c27dec69fd27e62bbe8ba4b6c4ccd8852c8c797b
Rafael Henrique <fel.rique70@gmail.com>
2021-11-25 09:47:27 -0300

    parte 5

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,22 +1,31 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	public DataSource dataSource;
+	
+	
 	public ConnectionFactory() {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("root");
+		
+		comboPooledDataSource.setMaxPoolSize(15);
+		
+		this.dataSource = comboPooledDataSource;
 	}
 	
 	
 
 	public Connection recuperarConexao() throws SQLException {
 
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		return this.dataSource.getConnection();
 
 	}
 	
